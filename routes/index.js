031e49cf9073757fa12b1d28562855c38b12bd62
DumilDarji <dumilwork54@gmail.com>
2025-02-25 14:29:57 +0530

    msg

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ var user = require("../Controller/usercontroller");
 
 router.get("/", user.index);
 router.post("/Add", user.Add);
-router.get("/one_data", user.Get_id);
+router.get("/get", user.Get_id);
 
 module.exports = router;
